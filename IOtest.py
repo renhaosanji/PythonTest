7876bcc5e8b813acc3a403b6fd933c8e2c04f139
renhaosanji@gmail.com <renhaosanji@gmail.com>
2016-05-22 16:13:54 +0900

    test1

---
+++ IOtest.py
@@ -0,0 +1,2 @@
+__author__ = 'Administrator'
+file = open('/Users/michael/test.txt', 'r')
---
+++ PythonGUI.py
@@ -0,0 +1,25 @@
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'a hello world GUI example.'
+
+from Tkinter import *
+
+class Application(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.helloLabel = Label(self, text='Hello, world!')
+        self.helloLabel.pack()
+        self.quitButton = Button(self, text='Quit', command=self.quit)
+        self.quitButton.pack()
+
+app = Application()
+#
+app.master.title('Hello World')
+#
+app.mainloop()
